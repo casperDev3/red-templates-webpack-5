3b6a865b5fff15a1d956850d22b2f8a99e3446da
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-03-11 17:49:32 +0200

    add code lesson

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 // Watchers for scss and html files
 import "./index.scss";
 import "./scripts/watchers/_pages"
-
 // Import modules
-import { calcSumNumbers, calcSubtractNumbers } from "./scripts/modules/calc";
-import { Elements } from "./scripts/modules/elements";
 
-// Import methods
-import { includeHTML } from "./scripts/methods/_includeComponents";
+// Import utils
+import { initialsBtn } from "./scripts/utils/personalData";
 
 // Bisiness logic
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  includeHTML();
-  console.log(calcSubtractNumbers(10, 5));
-  console.log(calcSumNumbers(10, 5));
-  const elements = new Elements();
-  elements.addElement('element 1');
-  elements.addElement('element 2');
-  elements.addElement('element 3');
-  console.log(elements);
+  initialsBtn();
 });
