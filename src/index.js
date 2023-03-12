b28cce1e962f515fc74fdf1fc1ca488f79e2ff05
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-03-12 12:07:17 +0200

    add lesson

--- src/index.js
+++ src/index.js
@@ -11,4 +11,30 @@ import { initialsBtn } from "./scripts/utils/personalData";
 
 document.addEventListener('DOMContentLoaded', () => {
   initialsBtn();
+
+  const PARAMS = {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    spaceBetween: 30,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+  }
+
+  const swiper = new Swiper('.test_slider', PARAMS);
 });
