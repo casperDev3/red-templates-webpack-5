6eb54d1e80ab69ba9fe2f64a5dbd629e212742d4
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-03-18 22:15:05 +0200

    lection

--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
 // Watchers for scss and html files
 import "./index.scss";
-import "./scripts/watchers/_pages"
+import "./scripts/watchers/_pages";
 
-// Import modules
-import { calcSumNumbers, calcSubtractNumbers } from "./scripts/modules/calc";
-import { Elements } from "./scripts/modules/elements";
+class Products {
+  // properties
+  #disconts = 0.22;
+  constructor(title, price, image, description) {
+    this.title = title;
+    this.price = price;
+    this.image = image;
+    this.description = description;
+    this.discontPrice = this.#getPriceWithDiscont();
+  }
+  // private method
+  #getPriceWithDiscont() {
+    return this.price * (1 - this.#disconts);
+  }
+  // getter
+  get discontValue() {
+    return this.#disconts;
+  }
+}
 
-// Import methods
-import { includeHTML } from "./scripts/methods/_includeComponents";
+class Electronics extends Products {
+  // currentYear = new Date().getFullYear();
+  constructor(title, price, image, description, category, year) {
+    super(title, price, image, description);
+    this.category = category;
+    this.year = year;
+    // this.age = this.getAge();
+  }
+  // metthod
+  getAge() {
+    return this.currentYear - this.year;
+  }
+}
 
-// Bisiness logic
-document.addEventListener('DOMContentLoaded', () => {
-  includeHTML();
-  console.log(calcSubtractNumbers(10, 5));
-  console.log(calcSumNumbers(10, 5));
-  const elements = new Elements();
-  elements.addElement('element 1');
-  elements.addElement('element 2');
-  elements.addElement('element 3');
-  console.log(elements);
-});
+class Books extends Products {
+  constructor(title, price, image, description, author) {
+    super(title, price, image, description);
+    this.author = author;
+  }
+}
+
+class Clothes extends Products {
+  constructor(title, price, image, description, size) {
+    super(title, price, image, description);
+    this.size = size;
+  }
+}
+
+// mac book pro
+const macBookPro = new Electronics(
+  "MacBook Pro",
+  2500,
+  "https://www.apple.com/v/macbook-pro-13/b/images/overview/compare/macbook_pro__f2x2j2y2w2.png",
+  "The best for developers",
+  "laptop",
+  2019
+);
+
+console.log(macBookPro);
