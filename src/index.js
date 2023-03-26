2db6fb6e1d1f40fe6cf232d6a0e2d536f193aba2
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-03-26 11:18:06 +0300

    add first part

--- src/index.js
+++ src/index.js
@@ -5,18 +5,37 @@ import "./scripts/watchers/_pages";
 // import modules
 import { Products } from "./scripts/modules/Products/Products";
 
+function deleteProductProcess(products) {
+  document.querySelectorAll(".btn_delete").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const PRODUCT_ID = btn.getAttribute("data-productId");
+      products.products = products.deleteProductById(
+        products.products,
+        PRODUCT_ID
+      );
+      console.log(products.products);
+      products.drawAllProducts();
+      deleteProductProcess();
+    });
+  });
+}
+
 // Bisiness logic
 document.addEventListener("DOMContentLoaded", async () => {
   let products = new Products();
   products.products = await products.getAllProducts();
   products.drawAllProducts();
-  console.log(
-    await products.createProduct({
-      title: "Test",
-      description: "Test",
-      price: 100,
-      category: "Test",
-      image: "Test",
-    })
-  );
+  document.querySelector("#input_count").addEventListener("change", () => {
+    products.getDataAndDisplayInBtn();
+  });
+  document
+    .querySelector("#show_count_post")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const PRODUCT_COUNT = products.getDataAndDisplayInBtn();
+      products.products = await products.getProductsByCount(PRODUCT_COUNT);
+      products.drawAllProducts();
+    });
+
+  deleteProductProcess(products);
 });
--- src/scripts/modules/Products/Products.js
+++ src/scripts/modules/Products/Products.js
@@ -21,7 +21,8 @@ export class Products {
             <h3>${product.title}</h3>
             <p>${product.description}</p>
             <p>$${product.price}</p>
-            <button class="btn btn-primary">Add to cart</button>
+            <button>Add to cart</button>
+            <button data-productId="${product.id}" class="btn_delete">Delete</button>
             </div>
         `;
     });
@@ -37,5 +38,27 @@ export class Products {
       .then((json) => {
         return json;
       });
+  }
+  getDataAndDisplayInBtn() {
+    // 1. Get value input by id
+    const count = document.querySelector("#input_count").value;
+    // 2. Show in Btn
+    document.querySelector("#count").innerHTML = count;
+    return count;
+  }
+  async getProductsByCount(count) {
+    return await fetch(`${this.#baseUrl}/products?limit=${count}`)
+      .then((res) => res.json())
+      .then((json) => {
+        return json;
+      });
+  }
+  deleteProductById(products, id) {
+    // await fetch(`${this.#baseUrl}/products/${id}`, {
+    //   method: "DELETE",
+    // })
+    //   .then((res) => res.json())
+    //   .then((json) => console.log(json));
+    return products.filter((product) => product.id != id);
   }
 }
