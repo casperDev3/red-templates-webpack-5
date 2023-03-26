839c955eb41cc36780e45fb2e5aa10bf36215749
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-03-26 11:50:32 +0300

    add second part lection

--- src/index.js
+++ src/index.js
@@ -5,7 +5,28 @@ import "./scripts/watchers/_pages";
 // import modules
 import { Products } from "./scripts/modules/Products/Products";
 
-function deleteProductProcess(products) {
+let products = new Products();
+
+async function getProducts() {
+  products.products = await products.getAllProducts();
+  products.drawAllProducts();
+  document.querySelector("#input_count").addEventListener("change", () => {
+    products.getDataAndDisplayInBtn();
+  });
+}
+
+async function getProductsByCount() {
+  document
+    .querySelector("#show_count_post")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const PRODUCT_COUNT = products.getDataAndDisplayInBtn();
+      products.products = await products.getProductsByCount(PRODUCT_COUNT);
+      products.drawAllProducts();
+    });
+}
+
+function deleteProcess() {
   document.querySelectorAll(".btn_delete").forEach((btn) => {
     btn.addEventListener("click", () => {
       const PRODUCT_ID = btn.getAttribute("data-productId");
@@ -15,27 +36,15 @@ function deleteProductProcess(products) {
       );
       console.log(products.products);
       products.drawAllProducts();
-      deleteProductProcess();
+      // recusive
+      deleteProcess();
     });
   });
 }
 
 // Bisiness logic
 document.addEventListener("DOMContentLoaded", async () => {
-  let products = new Products();
-  products.products = await products.getAllProducts();
-  products.drawAllProducts();
-  document.querySelector("#input_count").addEventListener("change", () => {
-    products.getDataAndDisplayInBtn();
-  });
-  document
-    .querySelector("#show_count_post")
-    .addEventListener("submit", async (e) => {
-      e.preventDefault();
-      const PRODUCT_COUNT = products.getDataAndDisplayInBtn();
-      products.products = await products.getProductsByCount(PRODUCT_COUNT);
-      products.drawAllProducts();
-    });
-
-  deleteProductProcess(products);
+  await getProducts();
+  await getProductsByCount();
+  deleteProcess();
 });
